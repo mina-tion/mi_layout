895c01c9acf01ee15ccb2b44b0a593ee9b761861
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2022-02-17 16:21:28 +0200

    fixes

--- src/containers/Private/Main/Content/Brand/index.tsx
+++ src/containers/Private/Main/Content/Brand/index.tsx
@@ -1,13 +1,41 @@
 import React, { FC, useEffect, useState } from 'react'
 
 import Banner from 'components/Banner'
-import NextArrow from 'components/Arrows/NextArrow'
-import PrevArrow from 'components/Arrows/PrevArrow'
 import brand from 'sources/images/brandBanner.png'
 import gradient from 'sources/images/gradients/banner.png'
 import thread from 'sources/images/threads/brand.png'
 
 import styles from './styles.module.scss'
+import Slider from 'react-slick'
+
+const settings = {
+  arrows: false,
+  edgeFriction: 1,
+  infinite: false,
+  swipeToSlide: true,
+  variableWidth: true,
+  accessibility: true,
+  focusOnSelect: true,
+  draggable: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 500,
+  rows: 1,
+
+  responsive: [
+    {
+      breakpoint: 620,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        variableWidth: false,
+
+        appendDots: (dots: any) => <ul> {dots} </ul>,
+      },
+    },
+  ],
+}
 
 const Brand: FC = () => {
   const [width, setWidth] = useState(document.documentElement.clientWidth)
@@ -23,17 +51,40 @@ const Brand: FC = () => {
     }
   }, [])
 
-
   return (
-    <div className={styles.bannerContainer}>
-      <Banner
-        imgUrl={brand}
-        title={'О бренде Xiaomi'}
-        text={`Компания Xiaomi была основана предпринимателем Лей Цзюнем в 2010 году под девизом - «Инновации для всех».
+    <div className={styles.container}>
+      <Slider {...settings} className={styles.slider}>
+        <div className={styles.bannerContainer}>
+          <Banner
+            imgUrl={brand}
+            title={'О бренде Xiaomi'}
+            text={`Компания Xiaomi была основана предпринимателем Лей Цзюнем в 2010 году под девизом - «Инновации для всех».
             Мы верим, что высококачественные продукты и передовые технологии должны быть доступны каждому.
             `}
-        isOrange={width >= 480}
-      />
+            isOrange={width >= 480}
+          />
+        </div>
+        <div className={styles.bannerContainer}>
+          <Banner
+            imgUrl={brand}
+            title={'О бренде Xiaomi'}
+            text={`Компания Xiaomi была основана предпринимателем Лей Цзюнем в 2010 году под девизом - «Инновации для всех».
+            Мы верим, что высококачественные продукты и передовые технологии должны быть доступны каждому.
+            `}
+            isOrange={width >= 480}
+          />
+        </div>
+        <div className={styles.bannerContainer}>
+          <Banner
+            imgUrl={brand}
+            title={'О бренде Xiaomi'}
+            text={`Компания Xiaomi была основана предпринимателем Лей Цзюнем в 2010 году под девизом - «Инновации для всех».
+            Мы верим, что высококачественные продукты и передовые технологии должны быть доступны каждому.
+            `}
+            isOrange={width >= 480}
+          />
+        </div>
+      </Slider>
       <img src={thread} className={styles.thread} alt="thread" />
       <img src={gradient} className={styles.gradient} alt="gradient" />
     </div>
